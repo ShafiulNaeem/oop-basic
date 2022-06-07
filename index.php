46c43b84d47e9fa2d392862147d4a2da34d40738
shafi <shafiul15-1052@diu.edu.bd>
2022-06-08 01:12:30 +0600

    static,constant

--- index.php
+++ index.php
@@ -8,18 +8,31 @@ class Person{
 	public $name;
 	public $age;
 	public $details;
+	const CITY = "Dhaka";
+	public static $myStaticProperty = 'Hyvor';
 
 	public function __construct(){
 		$details = new Details("09876543");
 		$this->details = $details->hello();
 	}
 
+
 	public function personName(){
-		echo "Person Name is:".$this->name."<br/>"."Details constructor data: ".$this->details."<br/>";
+
+		$data = self::getStatic();
+
+		echo "Person Name is:".$this->name."<br/>".
+			"Details constructor data: ".$this->details."<br/>".
+			"Static function data: ".$data."<br/>".
+			"Static data: ".self::$myStaticProperty."<br/>";
     }
 
+	public static function getStatic(){
+		return "It's a static function";
+	}
+
   	public function personAge($value){
-		echo "Person Age is:".$this->age=$value;
+		echo "Person Age is:".$this->age=$value."<br/>"."City Constant: ".Person::CITY;
    	}
 
 	public function __destruct(){
@@ -31,7 +44,7 @@ class Person{
 $personOne = new Person;
 $personOne->name="Ariful Islam";
 $personOne->personName();
-
+echo "<br/>";
 $personOne->personAge("18");
 
 
